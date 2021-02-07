f9bb36d7772d452d42813078702153efb04ee3d1
Simon Faltum <simon@anymate.io>
2021-02-07 19:44:39 +0100

    had some naming switched around

---
+++ Anymate.Dotnet.Template.Worker/Models/AnymateTask.cs
@@ -0,0 +1,15 @@
+﻿namespace Anymate.Dotnet.Template.Worker.Models
+{
+    public class AnymateTask
+    {
+        public long TaskId { get; set; } //The TaskId
+        public string Reason { get; set; } //The current Reason from the Task
+        public long RunId { get; set; } //The Id of the current Process Run
+        public string ProcessKey { get; set; } //The ProcessKey where the Task belong
+
+        /* 
+         * Add your own parameters here depending on how the Process is configured.
+         */
+
+    }
+}
---
+++ Anymate.Dotnet.Template.Worker/Models/TaskAction.cs
@@ -0,0 +1,54 @@
+﻿using Anymate.Dotnet.Template.Worker.Configuration;
+using System;
+
+namespace Anymate.Dotnet.Template.Worker.Models
+{
+    public class TaskAction
+    {
+        public TaskAction()
+        {
+        }
+
+        public TaskAction(long taskId, string reason = null, string comment = null, AnymateEndpoint endpoint = AnymateEndpoint.Solved, DateTimeOffset? activationDate = null,  int? overwriteSecondsSaved = null, int? overwriteEntries = null)
+        {
+            AnymateEndpoint = endpoint;
+            ActivationDate = activationDate;
+            TaskId = taskId;
+            Reason = reason;
+            Comment = comment;
+            OverwriteSecondsSaved = overwriteSecondsSaved;
+            OverwriteEntries = overwriteEntries;
+        }
+        public AnymateEndpoint AnymateEndpoint { get; set; } = AnymateEndpoint.Solved;
+        public DateTimeOffset? ActivationDate { get; set; } = null;
+        public long TaskId { get; set; }
+        public string Reason { get; set; }
+        public string Comment { get; set; }
+        public int? OverwriteSecondsSaved { get; set; } = null;
+        public int? OverwriteEntries { get; set; } = null;
+
+        public AnymateTaskAction GetAnymateTaskAction()
+        {
+            return new AnymateTaskAction()
+            {
+                TaskId = this.TaskId,
+                Comment = this.Comment,
+                OverwriteEntries = this.OverwriteEntries,
+                OverwriteSecondsSaved = this.OverwriteSecondsSaved,
+                Reason = this.Reason
+            };
+        }
+        public AnymateRetryTaskAction GetAnymateRetryTaskAction()
+        {
+            return new AnymateRetryTaskAction()
+            {
+                TaskId = this.TaskId,
+                Comment = this.Comment,
+                OverwriteEntries = this.OverwriteEntries,
+                OverwriteSecondsSaved = this.OverwriteSecondsSaved,
+                Reason = this.Reason,
+                ActivationDate = this.ActivationDate
+            };
+        }
+    }
+}
